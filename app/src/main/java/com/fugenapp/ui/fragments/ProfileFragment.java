e19cb35fbbefdb27a89c16f29b20e7a135bb61b2
nimal54 <35070950+nimal54@users.noreply.github.com>
2018-01-23 21:45:14 +0530

    #4; create profile fragment

---
+++ app/src/main/java/com/fugenapp/ui/fragments/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.fugenapp.ui.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fugenapp.R;
+
+/**
+ * Created by NIMAL on 23-01-2018.
+ */
+
+public class ProfileFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile,container,false);
+    }
+}
