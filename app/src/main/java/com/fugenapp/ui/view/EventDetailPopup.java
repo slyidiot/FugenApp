053d5bb3c2d05819a2f90fb19822a0532dbb5f62
vaisakhjnr <vaisakhjnr@gmail.com>
2018-02-13 09:28:00 +0530

    #18: Fixed Venue not updating bug. Added open source mit license. Release 1.0.1

--- app/src/main/java/com/fugenapp/ui/view/EventDetailPopup.java
+++ app/src/main/java/com/fugenapp/ui/view/EventDetailPopup.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Vaisakh J Nair
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.fugenapp.ui.view;
 
 import android.content.Context;
@@ -27,6 +51,7 @@ public class EventDetailPopup extends BlurPopupWindow {
     private TextView date;
     private TextView time;
     private TextView desc;
+    private TextView venue;
     private TextView contact;
 
     public EventDetailPopup(@NonNull Context context) {
@@ -53,12 +78,14 @@ public class EventDetailPopup extends BlurPopupWindow {
         time = view.findViewById(R.id.popup_event_time);
         desc = view.findViewById(R.id.popup_event_desc);
         contact = view.findViewById(R.id.popup_event_contact);
+        venue = view.findViewById(R.id.popup_event_venue);
 
         name.setText(event.name);
         date.setText(event.date);
         time.setText(event.time);
         desc.setText(event.desc);
         contact.setText("Contact: " + event.contact);
+        venue.setText(event.venue);
 
         int resID = getResources().getIdentifier(event.image, "drawable", FugenApp.getAppContext().getPackageName());
         image.setImageResource(resID);
